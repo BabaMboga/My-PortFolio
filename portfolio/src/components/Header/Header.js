9498562bde2b0bd70f6991dd88cb3181a3828273
Ayim William <odhisayim@gmail.com>
2023-09-21 08:36:07 +0300

    commented background colour

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='w-full h-[80px] flex items-center'>
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          {/* ============== logo ============== */}
+          <div className='flex items-center gap-[10px]'>
+            <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500]
+            rounded-full flex items-center justify-center'>
+              AW
+            </span>     
+
+            <div className='leading-[20px]'>
+              <h2 className='text-xl text-smallTextColor font-[700]'>Ayim William</h2>
+              <p></p>
+            </div>       
+          </div>
+
+        </div>
+      </div>
+    </header>
+    
   )
 }
 
