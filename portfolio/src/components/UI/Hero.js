d8a4eb1455a7ece239d692721189e885d676c204
Ayim William <odhisayim@gmail.com>
2023-09-25 21:26:07 +0300

    Hero progress - new button and hrefs

--- portfolio/src/components/UI/Hero.js
+++ portfolio/src/components/UI/Hero.js
@@ -12,7 +12,15 @@ const Hero = () => {
                     <h5 data-aos='fade-right' data-aos-duration='1500' className='text-headingColor font-[600]
                     text-[16px]'> Hello welcome</h5>
                     <h1 data-aos='fade-up' data-aos-duration='1500' className='text-headingColor font-[800]
-                    text-[1.8rem] sm:text-[40px]'>I'm Ayim William <br/> Software Engineer</h1>
+                    text-[1.8rem] sm:text-[40px] leading-[35px] sm:leading-[46px] mt-5'>I'm Ayim William <br/> Software Engineer</h1>
+                    <div data-aos='fade-up' data-aos-duration='1800' data-aos-delay='200'className='flex items-center gap-6 mt-7'>
+                        <a href="#contact">
+                            <button className='bg-primaryColor text-white font-[500] flex items-center gap-2
+                            hover:bg-smallTextColor ease-in duration-300 py-2 px-4 ronded-[8px]'><i class="ri-mail-send-fill"></i> Hire me</button>
+                        </a>
+                        <a href="#portfolio" className='text-smallTextColor font-[600] text-[16px] border-b border-solid
+                        border-smallTextColor'>See portfolio</a>
+                    </div>
                 </div>
                 {/* ============ hero left end ============ */}
             </div>
