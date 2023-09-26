c7689bbe788490389f3abd7e645fe547d41886a3
Ayim William <odhisayim@gmail.com>
2023-09-26 22:53:56 +0300

    nearly finished with Hero script

--- portfolio/src/components/UI/Hero.js
+++ portfolio/src/components/UI/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import heroImg from "../../assets/images/hero-removebg.png";
-import Countup from "react-countup";
+import CountUp from "react-countup";
 
 const Hero = () => {
   return (
@@ -54,55 +54,96 @@ const Hero = () => {
               data-aos-duration="1500"
               className="flex gap-2 text-headingColor mt-12 font-[500] text-[15px] leading-7 sm:pl-14 sm:pr-10"
             >
-                <span>
+              <span>
                 <i class="ri-apps-line"></i>
-                </span>
-              I am a passionate and highly motivated full stack software engineer with comprehensive training in
-              a wide array of cutting edge technologies. My skill set includes proficiency in:
+              </span>
+              I am a passionate and highly motivated full stack software
+              engineer with comprehensive training in a wide array of cutting
+              edge technologies. My skill set includes proficiency in:
             </p>
-            <div className='flex items-center gap-9 mt-14'>
-                <span className=" text-smallTextColor text-[15px] font-[600]">
-                     Follow me: 
-                </span>
-                <span>
-                    <a href='#linkedin' className="text-smallTextColor text-[18px] font-[600]">
-                    <i class="ri-linkedin-box-line"></i>
-                    </a>                    
-                </span>
-                <span>
-                    <a href='#github' className="text-smallTextColor text-[18px] font-[600]">
-                    <i class="ri-github-fill"></i> 
-                    </a>
-                </span>
-                <span>
-                    <a href="#twitter" className="text-smallTextColor text-[18px] font-[600]">
-                    <i class="ri-twitter-x-line"></i>
-                    </a>
-                </span>
-                <span>
-                    <a href="#wordpress" className="text-smallTextColor text-[18px] font-[600]">
-                    <i class="ri-wordpress-fill"></i>
-                    </a>
-                </span>
+            <div className="flex items-center gap-9 mt-14">
+              <span className=" text-smallTextColor text-[15px] font-[600]">
+                Follow me:
+              </span>
+              <span>
+                <a
+                  href="#linkedin"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-linkedin-box-line"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#github"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-github-fill"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#twitter"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-twitter-x-line"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#wordpress"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-wordpress-fill"></i>
+                </a>
+              </span>
             </div>
           </div>
           {/* ============ hero left end ============ */}
           {/* ============ hero img ============ */}
           <div className="basis-1/3 mt-10 sm:mt-0 ">
             <figure className="flex items-center justify-center">
-                <img src={heroImg} alt="" className="rounded-[8px]"/>
+              <img src={heroImg} alt="" className="rounded-[8px]" />
             </figure>
           </div>
           {/* ============ hero img end ============ */}
           {/* ================ hero content right ================ */}
-          <div className="md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0
-          md:flex-col md:justify-end md:text-end">
+          <div
+            className="md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0
+          md:flex-col md:justify-end md:text-end"
+          >
             <div className="mb-10">
-                <h2 className="text-headingColor font-[700] text-[32px]">
-                    <Countup start={0} end={6} duration={2} suffix="+" />
-                </h2>
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={6} duration={2} suffix="+" />
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Years of Experience
+              </h4>
             </div>
-
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={100} duration={2} suffix="%" />
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Success Rate
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={3} duration={2} suffix="+" />
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Projects
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={150} duration={2} suffix="+" />
+              </h2>
+              <h4 className="text-headingColor font-[700] text-[18px]">
+                Happy Clients
+              </h4>
+            </div>
           </div>
           {/* ================ hero content right end ================ */}
         </div>
