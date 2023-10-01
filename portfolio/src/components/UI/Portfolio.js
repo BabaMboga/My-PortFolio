1fc8927be2114ae8ea5488c46fbcc405ce7f4c49
Ayim William <odhisayim@gmail.com>
2023-10-01 14:54:31 +0300

    completed portfolio script

--- portfolio/src/components/UI/Portfolio.js
+++ portfolio/src/components/UI/Portfolio.js
@@ -1,14 +1,32 @@
-import React, {useState, useEFfect} from "react";
-import data from '../../assets/data/portfolioData'
+import React, { useState, useEffect } from "react";
+import data from "../../assets/data/portfolioData";
 
 const Portfolio = () => {
+  const [nextItems, setNextItems] = useState(6);
+  const [portfolios, setPortfolios] = useState(data);
+  const [selectTab, setSelectTab] = useState("all");
 
-    const [nextItems, setNextItems] = useState(6)
-    const [portfolios, setPortfolios] = useState(data)
+  const loadMoreHandler = () => {
+    setNextItems((prev) => prev + 3);
+  };
 
-    const loadMoreHandler = () => {
-        setNextItems(prev => prev + 3)
+  useEffect(() => {
+    if (selectTab === "all") {
+      setPortfolios(data);
     }
+
+    if (selectTab === "web-design") {
+      const filteredData = data.filter(
+        (item) => item.category === "Web Design"
+      );
+      setPortfolios(filteredData);
+    }
+
+    if (selectTab === "ux-design") {
+      const filteredData = data.filter((item) => item.category === "Ux");
+      setPortfolios(filteredData);
+    }
+  }, [selectTab]);
 
   return (
     <section id="portfolio">
@@ -21,54 +39,74 @@ const Portfolio = () => {
           </div>
 
           <div className="flex gap-3">
-            <button className="text-smallTextColor border border-solid border-smallTextColor 
-            py-2 px-4 rounded-[8px]">
-                All
+            <button
+              onClick={() => setSelectTab("all")}
+              className="text-smallTextColor border border-solid border-smallTextColor 
+            py-2 px-4 rounded-[8px]"
+            >
+              All
             </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor
-            py-2 px-4 rounded-[8px]">
-                Small
+            <button
+              onClick={() => setSelectTab("web-design")}
+              className="text-smallTextColor border border-solid border-smallTextColor
+            py-2 px-4 rounded-[8px]"
+            >
+              Web Design
             </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor
-            py-2 px-4 rounded-[8px]">
-                Medium
+            <button
+              onClick={() => setSelectTab("ux-design")}
+              className="text-smallTextColor border border-solid border-smallTextColor
+            py-2 px-4 rounded-[8px]"
+            >
+              UX Design
             </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor
+            {/* <button className="text-smallTextColor border border-solid border-smallTextColor
             py-2 px-4 rounded-[8px]">
                 Capstone
-            </button>
+            </button> */}
           </div>
         </div>
 
         <div className="flex items-center gap-4 flex-wrap mt-12">
-            {
-                portfolios?.slice(0, nextItems)?.map((portfolio, index) =>(
-                    <div key={index} data-aos="fade-zoom-in" data-aos-delay='50' data-aos-duration='1000'  className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]">
-                        <figure>
-                            <img className="rounded-[8px]" src={portfolio.imgUrl} alt=""/>
-                        </figure>
+          {portfolios?.slice(0, nextItems)?.map((portfolio, index) => (
+            <div
+              key={index}
+              data-aos="fade-zoom-in"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]"
+            >
+              <figure>
+                <img className="rounded-[8px]" src={portfolio.imgUrl} alt="" />
+              </figure>
 
-                        <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-[5]
-                        hidden group-hover:block">
-                            <div className="w-full h-full flex items-center justify-center">
-                                <button className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4
-                                rounded-[8px] font-[500] ease-in duration-200">See Details</button>
-                            </div>
-                        </div>
-                    </div>
-                ))
-            }
+              <div
+                className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-[5]
+                        hidden group-hover:block"
+              >
+                <div className="w-full h-full flex items-center justify-center">
+                  <button
+                    className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4
+                                rounded-[8px] font-[500] ease-in duration-200"
+                  >
+                    See Details
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
 
         <div className="text-center mt-6">
-
-            {
-                nextItems < portfolios.length && data.length > 6 && <button onClick={loadMoreHandler} className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4
-                rounded-[8px] font-[500] ease-in duration-200">
-                    Load More
-                </button> 
-            }
-            
+          {nextItems < portfolios.length && data.length > 6 && (
+            <button
+              onClick={loadMoreHandler}
+              className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4
+                rounded-[8px] font-[500] ease-in duration-200"
+            >
+              Load More
+            </button>
+          )}
         </div>
       </div>
     </section>
