4d620ce6f3e33fe6f801990d152a917470dc93ba
Ayim William <odhisayim@gmail.com>
2023-10-01 10:21:10 +0300

    added portfolio to app.js

---
+++ portfolio/src/components/UI/Portfolio.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <section id='portfolio'>
+        <div className='container'>
+            <div className='flex items-center justify-between flex-wrap'>
+                <div className='mb-7 sm:mb-0'>
+                    <h3 className='text-headingColor text-[2rem] font-[700]'>
+                        My Recent Projects
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Portfolio
