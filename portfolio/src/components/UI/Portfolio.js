8837d1d0f8e5823c990a25724af48462cebf5f03
Ayim William <odhisayim@gmail.com>
2023-10-01 12:58:52 +0300

    finished xml in portfolio script

--- portfolio/src/components/UI/Portfolio.js
+++ portfolio/src/components/UI/Portfolio.js
@@ -35,20 +35,28 @@ const Portfolio = () => {
         <div className="flex items-center gap-4 flex-wrap mt-12">
             {
                 data?.map((portfolio, index) =>(
-                    <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]">
+                    <div data-aos="fade-zoom-in" data-aos-delay='50' data-aos-duration='1000'  className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]">
                         <figure>
                             <img className="rounded-[8px]" src={portfolio.imgUrl} alt=""/>
                         </figure>
 
                         <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-[5]
                         hidden group-hover:block">
-                            <div>
-                                <button>See Details</button>
+                            <div className="w-full h-full flex items-center justify-center">
+                                <button className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4
+                                rounded-[8px] font-[500] ease-in duration-200">See Details</button>
                             </div>
                         </div>
                     </div>
                 ))
             }
+        </div>
+
+        <div className="text-center mt-6">
+            <button className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4
+            rounded-[8px] font-[500] ease-in duration-200">
+                Load More
+            </button>
         </div>
       </div>
     </section>
