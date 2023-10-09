623113358777ddb73f26781713269aa2cb7259dd
Ayim William <odhisayim@gmail.com>
2023-10-09 14:21:39 +0300

    finished on Header

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,70 +1,106 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from "react";
 
 const Header = () => {
-
-  const headerRef = useRef(null)
+  const headerRef = useRef(null);
 
   const stickyHeaderFunc = () => {
-    window.addEventListener('scroll', () => {
-      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-        headerRef.current.classList.add('sticky_header')
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky_header");
       } else {
-        headerRef.current.classList.remove('sticky_header')
+        headerRef.current.classList.remove("sticky_header");
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
+    stickyHeaderFunc();
 
-  }, [])
+    return window.removeEventListener("scroll", stickyHeaderFunc);
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const targetAttr = e.target.getAttribute("href");
+    const location = document.querySelector(targetAttr).offsetTop;
+
+    window.scrollTo({
+      top: location - 80,
+      left: 0,
+    });
+  };
 
   return (
-    <header ref={headerRef} className='w-full h-[80px] flex items-center'>
-      <div className='container'>
-        <div className='flex items-center justify-between'>
+    <header ref={headerRef} className="w-full h-[80px] flex items-center">
+      <div className="container">
+        <div className="flex items-center justify-between">
           {/* ============== logo ============== */}
-          <div className='flex items-center gap-[10px]'>
-            <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500]
-            rounded-full flex items-center justify-center'>
+          <div className="flex items-center gap-[10px]">
+            <span
+              className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500]
+            rounded-full flex items-center justify-center"
+            >
               AW
-            </span>     
+            </span>
 
-            <div className='leading-[20px]'>
-              <h2 className='text-xl text-smallTextColor font-[700]'>Ayim William</h2>
-              <p className='text-smallTextColor text-[14px] font-[500]'>
+            <div className="leading-[20px]">
+              <h2 className="text-xl text-smallTextColor font-[700]">
+                Ayim William
+              </h2>
+              <p className="text-smallTextColor text-[14px] font-[500]">
                 Personal
               </p>
-            </div> 
+            </div>
           </div>
           {/* =========== logo end ===========  */}
           {/* =========== menu start ===========  */}
-          <div className='menu'>
-            <ul className='flex items-center gap-10'>
-              <li><a className="text-smallTextColor font-[600]" href="#about">About</a></li>
-              <li><a className="text-smallTextColor font-[600]" href="#services">Services</a></li>
-              <li><a className="text-smallTextColor font-[600]" href="#portfolio">Portfolio</a></li>
-              <li><a className="text-smallTextColor font-[600]" href="#contact">Contact</a></li>
+          <div className="menu">
+            <ul className="flex items-center gap-10">
+              <li>
+                <a onclick={handleClick} className="text-smallTextColor font-[600]" href="#about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a onclick={handleClick} className="text-smallTextColor font-[600]" href="#services">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a onclick={handleClick} className="text-smallTextColor font-[600]" href="#portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a onclick={handleClick} className="text-smallTextColor font-[600]" href="#contact">
+                  Contact
+                </a>
+              </li>
             </ul>
           </div>
           {/* =========== menu end ===========  */}
           {/* =========== menu right ===========  */}
-          <div className='flex items-center gap-4'>
-            <button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid
+          <div className="flex items-center gap-4">
+            <button
+              className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid
             border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor
-            hover:text-white hover:font-[500] ease-in duration-[300]'>
-            <i class="ri-mail-send-line"></i>Let's Talk
+            hover:text-white hover:font-[500] ease-in duration-[300]"
+            >
+              <i class="ri-mail-send-line"></i>Let's Talk
             </button>
-            <span className='text-2xl text-smallTextColor md:hidden cursor-pointer'>
-            <i class="ri-menu-line"></i>
+            <span className="text-2xl text-smallTextColor md:hidden cursor-pointer">
+              <i class="ri-menu-line"></i>
             </span>
           </div>
-          {/* =========== menu end ===========  */} 
-
+          {/* =========== menu end ===========  */}
         </div>
       </div>
     </header>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
