d9cab0df1bb08d60dd11052d3acf76f5ee33f2d5
Ayim William <odhisayim@gmail.com>
2023-12-04 05:21:15 +0300

    ✨ feat: added node mailer server scipt

--- portfolio/server/server.js
+++ portfolio/server/server.js
@@ -6,3 +6,22 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+    const {name, email, subject, message } = req.body;
+
+    // create a nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    // Define email options
+    const mailOptions = {
+        from: 'your-email@gmail.com', // sender address
+        to: 'odhisayim@gmail.com', // recipient address
+    }
+})
