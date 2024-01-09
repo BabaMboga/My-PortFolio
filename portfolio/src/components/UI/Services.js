3d49a6070e7fa6f92922e5fe6ec566dcfb0a3ca3
Ayim William <odhisayim@gmail.com>
2024-01-09 06:09:48 +0300

    Finished working on services js

--- portfolio/src/components/UI/Services.js
+++ portfolio/src/components/UI/Services.js
@@ -6,20 +6,19 @@ import frontendImg from "../../assets/images/front-end.png";
 import backendImg from "../../assets/images/backend.png";
 import uiuxImg from "../../assets/images/design.png";
 import appsImg from "../../assets/images/apps.png";
-import cssImg from "../../assets/images/project-icons/css-3.png"
-import htmlImg from "../../assets/images/project-icons/html-5.png"
-import flaskImg from "../../assets/images/project-icons/icons8-flask.svg"
-import javascriptImg from "../../assets/images/project-icons/icons8-javascript.svg"
-import reactImg from "../../assets/images/project-icons/icons8-react.svg"
-import tailwindCssImg from "../../assets/images/project-icons/icons8-tailwindcss.svg"
-import javaImg from "../../assets/images/project-icons/java.png"
-import jsImg from "../../assets/images/project-icons/js.png"
-import pythonImg from "../../assets/images/project-icons/python.png"
-import reactImg2 from "../../assets/images/project-icons/react.png"
-import sqlImg from "../../assets/images/project-icons/sql-server.png"
+import cssImg from "../../assets/images/project-icons/css-3.png";
+import htmlImg from "../../assets/images/project-icons/html-5.png";
+import flaskImg from "../../assets/images/project-icons/icons8-flask.svg";
+import javascriptImg from "../../assets/images/project-icons/icons8-javascript.svg";
+import reactImg from "../../assets/images/project-icons/icons8-react.svg";
+import tailwindCssImg from "../../assets/images/project-icons/icons8-tailwindcss.svg";
+import javaImg from "../../assets/images/project-icons/java.png";
+import jsImg from "../../assets/images/project-icons/js.png";
+import pythonImg from "../../assets/images/project-icons/python.png";
+import reactImg2 from "../../assets/images/project-icons/react.png";
+import sqlImg from "../../assets/images/project-icons/sql-server.png";
 
 const Services = () => {
-
   // Create an array of icon elements
   const slideShowIcons = [
     <img src={cssImg} alt="CSS" />,
@@ -40,32 +39,35 @@ const Services = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow:3,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-  }
+  };
 
   return (
     <section id="services">
       <div className="container lg:pt-5">
         <div className="text-center">
           <h2 className="text-headingColor font-[800] text-[2.4 rem] mb-5">
-            What do I help
+            What do I help?
           </h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, uis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            As a seasoned developer, I offer expertise in a variety of
+            technologies, specializing in frontend and backend development,
+            UI/UX design and more. My skills span a variety of technologies,
+            frameworks and libraries like:
           </p>
-          <Slider {...sliderSettings} className= " flex items-center px-20 w-[50%] h-[40%] mt-4 mr-15 pt-5 ">
-              {slideShowIcons.map((icon, index) => (
-                <div key={index} className="icon-slide">
-                  {icon}
-                </div>
-              ))}
-            </Slider>
+          <Slider
+            {...sliderSettings}
+            className=" flex items-center px-20 mt-4 mr-15 pt-5 "
+          >
+            {slideShowIcons.map((icon, index) => (
+              <div key={index} className="icon-slide">
+                {icon}
+              </div>
+            ))}
+          </Slider>
         </div>
 
         <div className="flex flex-col justify-center sm:py-12">
@@ -89,12 +91,14 @@ const Services = () => {
                           className="text-primaryColor font-[700] mb-3 group-hover:text-white
                                             group-hover:font-[600] text-xl"
                         >
-                          Frontend Development
+                          Backend Development
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Consequartur, ad quas excepturi eius optio
-                          voluptatum.
+                          Building the backbone of robust applications with
+                          server-side technologies. From database management to
+                          server logic implementation, my backend development
+                          skills ensure the seamless fucntionality and
+                          performance of web applications.
                         </p>
                       </div>
                     </div>
@@ -130,9 +134,11 @@ const Services = () => {
                           UI/UX Design
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Consequartur, ad quas excepturi eius optio
-                          voluptatum.
+                          Crafting intuitive and visually stunning user
+                          experiences that resonate with users. From wireframes
+                          to interactive prototypes, my UI/UX design skills
+                          focus on creating engaging and user-centric digital
+                          products.
                         </p>
                       </div>
                     </div>
@@ -165,12 +171,13 @@ const Services = () => {
                           className="text-primaryColor font-[700] mb-3 group-hover:text-white
                                             group-hover:font-[600] text-xl"
                         >
-                          Backend Development
+                          Frontend Development
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Consequartur, ad quas excepturi eius optio
-                          voluptatum.
+                          Transforming designs into pixel-perfect, user-friendly
+                          interfaces using cutting-edge technologies. Elevate
+                          the user experience through responsive and visually
+                          appealing frontend development.
                         </p>
                       </div>
                     </div>
@@ -206,9 +213,11 @@ const Services = () => {
                           Agile Methodology
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Consequartur, ad quas excepturi eius optio
-                          voluptatum.
+                          Embracing an agile approach to project management,
+                          ensuring flexibility and collaboration. Iterative
+                          develpoment cycles, constant communication, and
+                          adaptability are key aspects of my methodology,
+                          resulting in succesful and client-centric outcomes.
                         </p>
                       </div>
                     </div>
