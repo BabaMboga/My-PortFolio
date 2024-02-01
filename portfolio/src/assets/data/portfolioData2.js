b5bfcbcbb051366611fa0826f5307b15b2b6eeb7
Ayim William <odhisayim@gmail.com>
2024-02-01 08:52:22 +0300

    updated portfolio data

---
+++ portfolio/src/assets/data/portfolioData2.js
@@ -0,0 +1,31 @@
+const portfolios = [
+    {
+        id: "",
+        imgUrl: ,
+        category: "",
+        title: "",
+        description:"",
+        technologies: [],
+        siteUrl: "",
+    },
+    {
+        id: "",
+        imgUrl: ,
+        category: "",
+        title: "",
+        description:"",
+        technologies: [],
+        siteUrl: "",
+    },
+    {
+        id: "",
+        imgUrl: ,
+        category: "",
+        title:"",
+        description:"",
+        technologies: [],
+        siteUrl: "",
+    },
+];
+
+export default portfolios;
