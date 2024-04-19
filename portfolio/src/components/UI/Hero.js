4d67ece2240f4aa43fee66a85ef58a869db7207f
Ayim William <odhisayim@gmail.com>
2024-04-19 19:16:22 +0300

    Changed projects prefix

--- portfolio/src/components/UI/Hero.js
+++ portfolio/src/components/UI/Hero.js
@@ -185,7 +185,7 @@ const Hero = () => {
             </div>
             <div className="mb-10">
               <h2 className="text-headingColor font-[700] text-[32px]">
-                <CountUp start={0} end={3} duration={2} suffix="+" />
+                <CountUp start={0} end={5} duration={2} suffix="+" />
               </h2>
               <h4 className="text-headingColor font-[600] text-[18px]">
                 Projects
